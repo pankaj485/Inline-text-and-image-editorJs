1261d3000619b65daa808533154c2275e7ea4da3
pankaj485 <pankajsamar2@gmail.com>
2022-10-27 11:46:41 +0530

    UX related UI improvements

--- imageEditorJs.js
+++ imageEditorJs.js
@@ -1,7 +1,7 @@
 class imageEditorJs {
 	static get toolbox() {
 		return {
-			title: "Pank Image",
+			title: "Inline Text And Image",
 			icon: '<svg width="17" height="15" viewBox="0 0 336 276" xmlns="http://www.w3.org/2000/svg"><path fill="#7D58E6" d="M291 150V79c0-19-15-34-34-34H79c-19 0-34 15-34 34v42l67-44 81 72 56-29 42 30zm0 52l-43-30-56 30-81-67-66 39v23c0 19 15 34 34 34h178c17 0 31-13 34-29zM79 0h178c44 0 79 35 79 79v118c0 44-35 79-79 79H79c-44 0-79-35-79-79V79C0 35 35 0 79 0z"/></svg>',
 		};
 	}
@@ -24,9 +24,9 @@ class imageEditorJs {
 							1: {
 								blocks: [
 									{
-										type: "paragraph",
+										type: "header",
 										data: {
-											text: "Text contents input...",
+											text: "Start with editing this header",
 										},
 									},
 								],
@@ -41,16 +41,16 @@ class imageEditorJs {
 			tools: {
 				header: {
 					class: Header,
-					shortcut: "CMD+SHIFT+H",
+					config: {
+						placeholder: "Enter a header",
+						levels: [2, 3, 4],
+						defaultLevel: 3,
+					},
 				},
 				nestedList: {
 					class: NestedList,
 					inlineToolbar: true,
 				},
-				// paragraphWithAlignment: {
-				// 	class: Paragraph,
-				// 	inlineToolbar: true,
-				// },
 			},
 		};
 
@@ -285,7 +285,7 @@ class imageEditorJs {
 
 		return {
 			imageUrl: imageUrl,
-			caption: captionElement.innerHTML,
+			textContent: captionElement.innerHTML,
 			imagePosition: imagePosition,
 			imageWidthPercentage: imageWidthPercentage,
 			isCaptionEmpty: captionElement.textContent === "",
